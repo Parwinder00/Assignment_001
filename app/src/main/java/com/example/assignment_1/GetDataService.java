9c6112334c33d75e5278af7f996e7cfb82b70c1f
Parwinder Singh <singhparwinder00@gmail.com>
2019-09-07 12:15:11 -0700

    second

---
+++ app/src/main/java/com/example/assignment_1/GetDataService.java
@@ -0,0 +1,15 @@
+package com.example.assignment_1;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+   /* @GET("Vy2abloQD")
+    Call<List<Pokemon>> getAllPokemon();
+*/
+    @GET("E14trR2lD")
+    Call<Pokemonpojo> getAllPokemon();
+}
